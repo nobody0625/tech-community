24f7a646443a178fd5ef31b70c3a4383db49dae7
nobody0625 <nobody06070401@outlook.com>
2025-06-19 00:10:33 +0800

    feat: 添加新页面的路由数组项

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,21 +1,45 @@
 import type { RouteObject } from "react-router-dom";
-import BasicLayout  from "../layouts/BasicLayout/BasicLayout";
-
+import BasicLayout from "../layouts/BasicLayout/BasicLayout";
+import HomePage from "../pages/HomePage";
+import ActivityPage from "../pages/global_nav/ActivityPage";
+import ArticlePage from "../pages/global_nav/ArticlePage";
+import CoursePage from "../pages/global_nav/CoursePage";
+import DocumentPage from "../pages/global_nav/DocumentPage";
+import QAPage from "../pages/global_nav/QAPAge";
 
 export const routes: RouteObject[] = [
   {
     path: "/",
     element: <BasicLayout />,
     children: [
-      // {
-      //   index: true,
-      //   element: <HomePage />,
-      // },
-    //   {
-    //     path: "/article/:id",
-    //     element: <Article />,
-    //   },
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/activity",
+        element: <ActivityPage />,
+      },
+      {
+        path: "/article",
+        element: <ArticlePage />,
+      },
+      {
+        path: "/course",
+        element: <CoursePage />,
+      },
+      {
+        path: "/document",
+        element: <DocumentPage />,
+      },
+      {
+        path: "/qa",
+        element: <QAPage />,
+      },
+      //   {
+      //     path: "/article/:id",
+      //     element: <Article />,
+      //   },
     ],
   },
-
 ];
