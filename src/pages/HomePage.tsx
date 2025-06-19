77b2234b39171032e9768e36a6b72e1f42284cf7
nobody0625 <nobody06070401@outlook.com>
2025-06-19 23:08:30 +0800

    feat: 编写HomePage组件

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,119 @@
+import { Alert, Carousel, ConfigProvider, Image } from "antd";
 import React from "react";
+import "./HomePage.css";
 
 const HomePage: React.FC = () => {
   return (
     <>
-      <h1>home</h1>
+      <ConfigProvider
+        theme={{
+          components: {
+            Carousel: {
+              dotOffset: 25,
+            },
+          },
+        }}
+      >
+        <div id="homePage">
+          {/* 轮播图 */}
+          <div className="top">
+            <div className="left" style={{ width: "800px", height: "350px" }}>
+              <Carousel
+                autoplay={{ dotDuration: true }}
+                autoplaySpeed={5000}
+                effect="fade"
+              >
+                <div>
+                  <div>
+                    <Image
+                      preview={false}
+                      width={800}
+                      height={350}
+                      src="public\images\image1.png"
+                      style={{
+                        display: "block",
+                      }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <Image
+                      preview={false}
+                      width={800}
+                      height={350}
+                      src="public\images\image2.png"
+                      style={{
+                        display: "block",
+                      }}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div>
+                    <Image
+                      preview={false}
+                      width={800}
+                      height={350}
+                      src="public\images\image3.png"
+                      style={{
+                        display: "block",
+                      }}
+                    />
+                  </div>
+                </div>
+              </Carousel>
+            </div>
+            <div className="right">
+              <div>
+                <Image
+                  preview={false}
+                  src="public\images\image1.png"
+                  height={110}
+                  style={{
+                    display: "block",
+                  }}
+                />
+              </div>
+              <div>
+                <Image
+                  preview={false}
+                  src="public\images\image2.png"
+                  height={110}
+                  style={{
+                    display: "block",
+                  }}
+                />
+              </div>
+              <div>
+                <Image
+                  preview={false}
+                  src="public\images\image3.png"
+                  height={110}
+                  style={{
+                    display: "block",
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+          {/* 文章与排行榜 */}
+          <div className="bottom">
+            {/* 小贴士 */}
+            <div className="tips">
+              <Alert
+                message="Alert Message Text"
+                type="success"
+                closable
+              />
+            </div>
+            <div className="left"></div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </ConfigProvider>
     </>
-    );
-}
+  );
+};
 
 export default HomePage;
