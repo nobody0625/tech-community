ba49199130fae6b045ff4eab8d532ad0d52a214c
nobody0625 <nobody06070401@outlook.com>
2025-06-22 17:56:34 +0800

    feat: 为评论添加点赞和点踩功能

--- src/components/comment/CommentItem.tsx
+++ src/components/comment/CommentItem.tsx
@@ -1,9 +1,9 @@
 import Comment from "@ant-design/compatible/lib/comment";
 import { Avatar, Tooltip } from "antd";
-import React from "react";
+import React, { createElement, useState } from "react";
 import CommentInput from "./CommentInput";
 import moment from "moment";
-import { UserOutlined } from "@ant-design/icons";
+import { DislikeFilled, DislikeOutlined, LikeFilled, LikeOutlined, UserOutlined } from "@ant-design/icons";
 
 interface CommentType {
   id: number;
@@ -38,6 +38,23 @@ const CommentItem: React.FC<CommentItemProps> = ({
 }) => {
   // 限制评论层数
   const canReply = depth < 4;
+
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+  const [action, setAction] = useState<string | null>(null);
+
+  const like = () => {
+    setLikes(1);
+    setDislikes(0);
+    setAction("liked");
+  };
+
+  const dislike = () => {
+    setLikes(0);
+    setDislikes(1);
+    setAction("disliked");
+  };
+
   return (
     <>
       <Comment
@@ -55,6 +72,16 @@ const CommentItem: React.FC<CommentItemProps> = ({
           </Tooltip>
         }
         actions={[
+          <span onClick={like}>
+            {createElement(action === "liked" ? LikeFilled : LikeOutlined)}
+            <span className="comment-action">{likes}</span>
+          </span>,
+          <span onClick={dislike}>
+            {React.createElement(
+              action === "disliked" ? DislikeFilled : DislikeOutlined
+            )}
+            <span className="comment-action">{dislikes}</span>
+          </span>,
           canReply && (
             <span key="reply" onClick={() => setActiveReplyId?.(comment.id)}>
               回复
